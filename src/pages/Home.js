83b0ed5d5830731cd81673aa17deecf845c221da
RyBuilds <Ryan@Registe.me>
2022-11-08 17:07:20 +0000

    completed movie card component and horizontal scroll, and click functionality

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,15 @@
 import Main from "../components/Main";
+import Row from "../components/Row";
+import requests from "../Requests";
 
 function Home() {
   return (
     <div>
       <Main />
+      <Row rowId="1" title="Popular" fetchURL={requests.requestPopular} />
+      <Row rowId="2" title="Top Rated" fetchURL={requests.requestTopRated} />
+      <Row rowId="3" title="Trending" fetchURL={requests.requestTrending} />
+      <Row rowId="4" title="Upcomming" fetchURL={requests.requestUpcoming} />
     </div>
   );
 }
