a47e75a5ab7dc26b911942c87cad4a4f087a1d27
RyBuilds <Ryan@Registe.me>
2022-11-10 22:54:05 +0000

    small bug fixes with optional chaining

--- src/components/Main.js
+++ src/components/Main.js
@@ -13,8 +13,6 @@ function Main() {
     });
   }, []);
 
-  console.log(movie);
-
   const truncateString = (string, number) => {
     if (string?.length > number) {
       return string.slice(0, number) + `...`;
